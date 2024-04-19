684ed9e5e75fda61429f3a987631372f3361616c
Timothy Liao <tim80411@oneclass.tw>
2024-04-19 21:10:41 +0800

    feat: modify git

--- macOS/mongo_setup.sh
+++ macOS/mongo_setup.sh
@@ -2,8 +2,9 @@
 echo "sleeping for 5 seconds"
 sleep 5
 
-# host需填寫hostname，原因是若使用docker network，本機無法解讀，需要取得hostname可用hostname -s
+# 為了確保mongo彼此都知道Domain Name，mongo的networkMode要設定為host
 echo mongo_setup.sh time now: `date +"%T" `
+HOSTNAME=$(hostname -s)
 mongosh --host $HOSTNAME:27017 <<EOF
   var cfg = {
     "_id": "rs0",
